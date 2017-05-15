afa3fa17b8739285872f3b0f5557568c44df4d04
Marcin Wieczorek <marcin@marcin.co>
2017-05-15 12:13:22 +0200

    Fixed memcpy warning

--- libc/string/memcpy.c
+++ libc/string/memcpy.c
@@ -1,8 +1,8 @@
 #include <string.h>
 
 void *memcpy (void *src, const void *dst, size_t no_bytes) {
-    unsigned char *dest = dst;
-    unsigned char *source = src;
+    char *dest = (void*) dst;
+    char *source = src;
 
     for(int i = 0; i < no_bytes; i++) {
         *(dest + i) = *(source + i);
