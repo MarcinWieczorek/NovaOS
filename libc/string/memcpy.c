41b0f0af9baaa5d773cb2ad76484f0ac868e6b7e
Marcin Wieczorek <marcin@marcin.co>
2017-12-11 16:29:22 +0100

    Fixed casting memcpy

--- libc/string/memcpy.c
+++ libc/string/memcpy.c
@@ -1,8 +1,8 @@
 #include <string.h>
 
 void *memcpy(void *dst, const void *src, size_t no_bytes) {
-    char *dest = (void*) dst;
-    char *source = src;
+    char *dest = dst;
+    char *source = (void*) src;
 
     for(int i = 0; i < no_bytes; i++) {
         *(dest + i) = *(source + i);
